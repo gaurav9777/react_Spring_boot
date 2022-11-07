79540e8af59cd3e2a41f22d6dc10a46c3834d3d7
lusifer97 <sameerverma01001@gmail.com>
2022-11-07 11:30:40 +0530

    Added add Product funtionality

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import AddProduct from './Component/AddProduct';
+import EditProduct from './Component/EditProduct';
+import Home from './Component/Home';
+import Navbar from './Component/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar></Navbar>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/addProduct' element={<AddProduct/>}></Route>
+      <Route path='/editProduct' element={<EditProduct/>}></Route>
+    </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Component/EditProduct.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const EditProduct = () => {
+  return (
+    <>Edit Product Page 
+    </>
+  )
+}
+
+export default EditProduct
